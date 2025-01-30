de15f16ddaa199a7e915a687b998a5331664f0b6
Iury Nogueira <ikdn@ic.ufal.br>
2025-01-30 09:47:58 -0300

    .

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,19 +1,16 @@
+require('dotenv').config();
+console.log(process.env.DATABASE_URL); // Verifique se a variável foi carregada corretamente
+
 const { Sequelize } = require('sequelize');
-require('dotenv').config(); // Carregar variáveis do .env
-
-const { DATABASE_URL, PGUSER, PGPASSWORD, PGHOST, PGPORT, PGDATABASE } = process.env;
-
-const connectionString = DATABASE_URL || `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}`;
-
-const sequelize = new Sequelize(connectionString, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    logging: false, // Desabilitar log de SQL (opcional)
+    logging: false,
     dialectOptions: {
         ssl: {
             require: true,
-            rejectUnauthorized: false // You may need to adjust this based on your SSL configuration
+            rejectUnauthorized: false
         }
     }
 });
 
-module.exports = sequelize; // Exportar a configuração
+module.exports = sequelize;
